689bc8fdc3f86f2b6c8a6c64baa0df75522685c0
Pedro <pedrodanilo48@gmail.com>
2026-02-17 23:03:13 -0300

    "First commit"

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./scripts/**/*.{js,ts}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
